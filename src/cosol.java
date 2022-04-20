90b3103757b8ba9e0f2064a9b8133bb3b33c8bd5
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2022-04-20 21:05:36 +0200

    Implement Conditionals, Labels, Loops

--- src/cosol.java
+++ src/cosol.java
@@ -5,7 +5,10 @@ public class cosol {
 	public static List<String> StrStck = new ArrayList<>(); // List for Strings
 	public static List<Integer> MathStck = new ArrayList<>(); // List for Performing Math operations (integers)
 	public static List<Integer> CtrlStck = new ArrayList<>(); // List for implementing Control-Flow
-	public static Integer LastMathPop = 0;
+	public static Hashtable<String, String> _Labels = new Hashtable<>(); // HashTable for storing Labels with their names
+	public static String _NextLabelname = ""; // String for storing the Name of the Next Label
+	public static Integer _LoopIndex = 0; // Integer for storing the Loop-Index of a given loop
+	public static Integer LastMathPop = 0; // Integer for storing the last value that was popped from the Math Stack
 	
 	public static void main(String[] args) {
 		try {
@@ -20,10 +23,6 @@ public class cosol {
 			System.out.println(">?");
 			System.exit(0);
 		}
-	}
-	
-	public static boolean isNum(char num) {
-		return num == '0' || num == '1' || num == '2' || num == '3' || num == '4' || num == '5' || num == '6' || num == '7' || num == '8' || num == '9' || num == '0';
 	}
 	
 	public static Integer mathPop() {
@@ -52,14 +51,24 @@ public class cosol {
 		return null;
 	}
 	
+	
 	public static void interpret(String instruction) {
+		// defining Local Variables
 		char[] instructionChar = instruction.toCharArray();
 		String fullStr = ""; // String for collecting Strings
 		boolean fillStr = false; // Boolean for marking when to collect Strings
 		String fullMath = ""; // String for collecting Integers
-		boolean fillMath = false; // String for marking when to collect Integers
+		boolean fillMath = false; // Boolean for marking when to collect Integers
+		String fullCond = ""; // String for collecting Conditional Expressions
+		boolean fillCond = false; // Boolean for marking when to collect Conditional Expressions
+		String fullLbl = ""; // String for Collecting Labels
+		boolean fillLbl = false; // Boolean for marking when to collect Labels
+		String fullLoop = ""; // String for Collecting Loops
+		boolean fillLoop = false; // Boolean for marking when to collect Loops
 		int stckIndex = 0; // Integer for marking which Stack should be addressed
 		
+		
+		// Actually interpreting the instruction
 		for (int i = 0; i < instructionChar.length; i++) {
 			// Ignore Whitespace
 			if (instructionChar[i] == ' ') {
@@ -97,6 +106,56 @@ public class cosol {
 					break;
 				}
 			}
+			// Conditional Collection
+			if (fillCond && instructionChar[i] != '(' && instructionChar[i] != ')') {
+				fullCond += instructionChar[i];
+			}
+			if (instructionChar[i] == '(' && !fillCond) {
+				fillCond = true;
+			}
+			else if (instructionChar[i] == ')' && fillCond) {
+				fillCond = false;
+				fullCond = "";
+			}
+			// Label Collection
+			if (fillLbl && instructionChar[i] != '{' && instructionChar[i] != '}') {
+				fullLbl += instructionChar[i];
+			}
+			if (instructionChar[i] == '{' && !fillLbl) {
+				fillLbl = true;
+			}
+			else if (instructionChar[i] == '}' && fillLbl) {
+				fillLbl = false;
+				if (_NextLabelname != "") {
+					_Labels.put(_NextLabelname, fullLbl);
+					fullLbl = "";
+					_NextLabelname = "";
+				}
+				else {
+					System.out.println("\"?\":{}");
+					System.exit(0);
+					break;
+				}
+				
+			}
+			
+			// Loop Collection and Execution
+			if (fillLoop && instructionChar[i] != '[' && instructionChar[i] != ']') {
+				fullLoop += instructionChar[i];
+			}
+			if (instructionChar[i] == '[' && !fillLoop) {
+				fillLoop = true;
+			}
+			else if (instructionChar[i] == ']' && fillLoop) {
+				fillLoop = false;
+				// Loop Execution
+				for (int j = 0; j < _LoopIndex; j++) {
+					interpret(fullLoop);
+				}
+				fullLoop = "";
+			}
+			
+			
 			// Math Operations
 			// Pops the first to Values of the MathStack, 
 			// Does the specified operation, e.g: SecondValue - FirstValue
@@ -123,19 +182,60 @@ public class cosol {
 					MathStck.add(x / y);
 				}
 			}
+			
+			
 			// Comparisons
-			if (!fillStr && !fillMath) {
-				// Conditional jump function
-				if (instructionChar[i] == '\'') {
-					int num = ctrlPop();
-					if (num != 0) {
-						i = MathStck.get(MathStck.size() - 1) - 1; // Reset the for Loop to the desired position -> since i would instantly increase, we subtract one right away, so we get the correct value.
-						continue;
+			// (for filling the Control Stack: Branching will be in the standard operations)
+			if (!fillStr && !fillMath && fillCond) {
+				if (instructionChar[i] == '=') {
+					int x = mathPop();
+					int y = mathPop();
+					
+					if (x == y) {
+						CtrlStck.add(1);
+					}
+					else {
+						CtrlStck.add(0);
+					}
+				}
+				if (instructionChar[i] == '<') {
+					int x = mathPop();
+					int y = mathPop();
+					
+					if (x <= y) {
+						CtrlStck.add(1);
+					}
+					else {
+						CtrlStck.add(0);
+					}
+				}
+				if (instructionChar[i] == '>') {
+					int x = mathPop();
+					int y = mathPop();
+					
+					if (x >= y) {
+						CtrlStck.add(1);
+					}
+					else {
+						CtrlStck.add(0);
+					}
+				}
+				if (instructionChar[i] == '!') {
+					int x = mathPop();
+					int y = mathPop();
+					
+					if (x != y) {
+						CtrlStck.add(1);
+					}
+					else {
+						CtrlStck.add(0);
 					}
 				}
 			}
+			
+			
 			// Standard Operations
-			if (!fillStr && !fillMath) {
+			if (!fillStr && !fillMath && !fillCond) {
 				// Print Function
 				if (instructionChar[i] == '.') {
 					System.out.println(strPop());
@@ -146,7 +246,15 @@ public class cosol {
 				}
 				// Shift to Control Function
 				if (instructionChar[i] == '>') {
-					CtrlStck.add(mathPop());
+					int num = mathPop();
+					if (num == 0 || num == 1) {
+						CtrlStck.add(num);
+					}
+					else {
+						System.out.println(">>?");
+						System.exit(0);
+						break;
+					}
 				}
 				// Try Shift to Math Function
 				if (instructionChar[i] == '¬') {
@@ -220,23 +328,76 @@ public class cosol {
 					else {
 						System.out.println("|?");
 					}
-				} 
-				// Goto Function
-				if (instructionChar[i] == '!') {
+				}
+				// Set Loop-Index
+				if (instructionChar[i] == ';') {
+					_LoopIndex = mathPop() - 1;
+				}
+				// Set next Label-Name
+				if (instructionChar[i] == ':') {
+					_NextLabelname = strPop();
+				}
+				// Conditional Jump to Label Function
+				if (instructionChar[i] == '\'') {
+					if (CtrlStck.get(CtrlStck.size() - 1) == 1) {
+						try {
+							interpret(_Labels.get(strPop()));
+						}
+						catch (Exception ex) {
+							System.out.println("{?}");
+							System.exit(0);
+							break;
+						}
+					}
+					else {
+						continue;
+					}
+				}
+				// Jump to Label Function
+				if (instructionChar[i] == '^') {
 					try {
-						i = MathStck.get(MathStck.size() - 1) - 1; // Reset the for Loop to the desired position -> since i would instantly increase, we subtract one right away, so we get the correct value.
+						interpret(_Labels.get(strPop()));
+					}
+					catch (Exception ex) {
+						System.out.println("{?}");
+						System.exit(0);
+						break;
+					}
+				}
+				// Conditional Goto Function
+				if (instructionChar[i] == '!') {
+					if (CtrlStck.get(CtrlStck.size() - 1) == 1) {
+						try {
+							i = mathPop() - 1; // Reset the for Loop to the desired position -> since i would instantly increase, we subtract one right away, so we get the correct value.
+							continue;
+						}
+						catch (Exception ex) {
+							System.out.println("!?");
+							System.exit(0);
+							break;
+						}
+					}
+					else {
+						continue;
+					}
+				}
+				// Goto Function
+				if (instructionChar[i] == '¨') {
+					try {
+						i = mathPop() - 1; // Reset the for Loop to the desired position -> since i would instantly increase, we subtract one right away, so we get the correct value.
 						continue;
 					}
 					catch (Exception ex) {
-						System.out.println("!?");
+						System.out.println("¨?");
 						System.exit(0);
 						break;
 					}
 				}
 			}
 		}
-		System.out.println(StrStck);
-		System.out.println(MathStck);
-		System.out.println(CtrlStck);
+		//System.out.println(StrStck);
+		//System.out.println(MathStck);
+		//System.out.println(CtrlStck);
+		//System.out.println(_Labels);
 	}
 }
