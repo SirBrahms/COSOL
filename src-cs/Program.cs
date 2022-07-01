8efeb958d2f4eecd0b7d67d70490f2e1adef7bdb
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2022-07-01 21:26:15 +0200

    Add virtual Token class

--- src-cs/Program.cs
+++ src-cs/Program.cs
@@ -5,8 +5,69 @@ namespace Cosol
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Token abc = new Token(123, Test);
+
+            Console.WriteLine(abc.Value);
+            abc.OnCall();
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("Test");
+            return;
         }
     }
+
+    # region Tokens
+    
+    class Token
+    {
+        protected object? _Value;
+        protected Action? _OnCall;
+    
+        public virtual object Value 
+        {
+            get
+            {
+                if (_Value != null)
+                    return _Value;
+                else
+                    throw new NullReferenceException("No Value was set");
+            }
+            set
+            {
+                {
+                    _Value = (object) value;
+                }
+            }
+        }
+
+        public virtual Action OnCall
+        {
+            get
+            {
+                if (_OnCall != null)
+                    return _OnCall;
+                else
+                    throw new NullReferenceException("No Action was set");
+            }
+            set
+            {
+                {
+                    _OnCall = (Action) value;
+                }
+            }
+        }
+
+        public Token(object Val, Action OnCall)
+        {
+            this.Value = Val;
+            this.OnCall = OnCall;
+        }
+    }
+
+    
+
+    # endregion
 }
 
