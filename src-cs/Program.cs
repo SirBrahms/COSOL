3686b6d56bb83de113a09ea30653b05db474b126
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2022-08-25 20:17:29 +0200

    Restore Reading and Console functionality

--- src-cs/Program.cs
+++ src-cs/Program.cs
@@ -2,9 +2,51 @@
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] Args)
         {
-            Console.WriteLine("Hello, World!");
+            if (Args.Length == 0) 
+            {
+                try 
+                {
+                    while (true) 
+                    {
+                        Console.Write("> ");
+                        string? Instruction;
+                        if ((Instruction = Console.ReadLine()) == null)
+                            return;
+                        Interpret(Instruction);
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(">?");
+                    Environment.Exit(0);
+                }
+            }
+            else 
+            {
+                // Read file supplied in args[0]
+                try 
+                {
+                    StreamReader Reader = new StreamReader(Args[0]);
+                    string FullString;
+                    FullString = Reader.ReadToEnd();
+
+                    FullString.Replace("/n", "");
+                    Interpret(FullString);
+                } 
+                catch (Exception) 
+                {
+                    Console.WriteLine("File not found!");
+                    Environment.Exit(1);
+                }
+                
+            }
+        }
+
+        static void Interpret(string FullString)
+        {
+            Console.WriteLine(FullString);
         }
     }
 }
