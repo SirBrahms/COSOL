51a9597bcb1bd3ccd6db7d9935009cfa20646e77
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2022-08-25 21:24:41 +0200

    Implement Tokenizing Method BundleChars()

--- src-cs/Program.cs
+++ src-cs/Program.cs
@@ -2,6 +2,14 @@
 {
     class Program
     {
+        // char[] -> Form in the char[]
+        // string -> Concatonated Form
+        public static Dictionary<string, char[]> Patterns = new Dictionary<string, char[]>()
+        {
+            {"->", new char[] {'-', '>'}}, // LARROW
+            {"<-", new char[] {'<', '-'}}, // RARROW
+        };
+
         static void Main(string[] Args)
         {
             if (Args.Length == 0) 
@@ -50,9 +58,40 @@
             BundleChars(Chars);
         }
 
-        static string BundleChars(char[] Chars)
+        static string[] BundleChars(char[] Chars)
         {
-            return "";
+            List<string> Result = new List<string>();
+            
+            
+            for (int i = 0; i < Chars.Length; i++)
+            {
+                bool AddChar = true;
+
+                foreach (KeyValuePair<string, char[]> KVP in Patterns)
+                {
+                    string Pattern = KVP.Key;
+
+                    if (Chars.Skip(i).Take(Patterns[Pattern].Length).SequenceEqual(Patterns[Pattern])) // Find the Sequence (Pattern)
+                    {
+                        Result.Add(Pattern); // Add the Pattern to the Result
+
+                        AddChar = false; // Skip the next character
+                    }
+                }
+
+                if (AddChar)
+                {
+                    Result.Add(Chars[i].ToString()); // If the Sequence wasn't found, just add the current char to Result
+                }
+                else
+                {
+                    i++; // If the sequence was found, skip the next char
+                    AddChar = true; // Allow for more chars to be added
+                }
+                
+            }
+
+            return Result.ToArray();
         }
     }
 }
